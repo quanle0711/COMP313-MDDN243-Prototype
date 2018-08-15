c25114fa1549beae807c4ac5e07a3ff2fa4e071b
lequan1 <lequan1@ecs.vuw.ac.nz>
2018-08-16 10:21:54 +1200

    readded under git ignore

---
+++ Assets/Scripts/ClickButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickButton : MonoBehaviour {
+	// Update is called once per frame
+	MeshRenderer MR;
+	void OnMouseDown() {
+		MR = GetComponent<MeshRenderer> ();
+		gameObject.transform.GetChild(0).gameObject.SetActive(true);
+		MR.enabled = false;
+	}
+}
---
+++ Assets/Scripts/GoldScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class GoldScript : MonoBehaviour
+{
+
+    public int gold;
+    public Text goldText;
+    // Use this for initialization
+    void Start()
+    {
+        gold = 150;
+        goldText.text = gold.ToString ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void addGold(int goldToAdd)
+    {
+        gold += goldToAdd;
+        goldText.text = gold.ToString();
+    }
+
+    public void subGold(int goldToSub)
+    {
+        if (gold - goldToSub < 0) {
+            Debug.Log("Not enough money");
+        } else {
+            gold -= goldToSub;
+            goldText.text = gold.ToString();
+            
+        }   
+    }
+}
